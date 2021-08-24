c67feb9c6857ae96f1250ae689d52491824d7dca
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-08-24 11:35:23 +0800

    docs: add source link & explaining how to use icons

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -2,17 +2,26 @@
   <div class="fixed top-0 left-0 lg:left-64 right-0 h-14 bg-white border-b border-gray-200 flex flex-row items-center justify-between px-5">
     <h3 class="text-sm font-medium text-gray-600">Welcome, Nur</h3>
     <div class="flex">
+      <div class="group flex items-center mr-3 cursor-pointer" @click="viewSource()">
+        <span class="text-sm  text-gray-600 group-hover:text-gray-800">GitHub</span>
+        <external-link-icon-o class="ml-1 w-4 h-4 text-gray-600 group-hover:text-gray-800" />
+      </div>
       <div class="relative flex justify-center items-center w-6 h-6 rounded-full bg-gray-200">
         <bell-icon-s class="w-3.5 h-3.5 text-gray-600" />
         <div class="absolute -top-0.5 -right-0.5 w-2.5 h-2.5 bg-blue-400 rounded-full" />
       </div>
-      <img src="@/assets/avatar.png" alt="Avatar" class="ml-2 w-6 h-6 rounded-full">
+      <img src="@/assets/avatar.png" alt="Avatar" class="ml-3 w-6 h-6 rounded-full">
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  methods: {
+    viewSource() {
+      window.open('https://github.com/ngekoding/admin-panel', '_blank')
+    }
+  }
 }
 </script>
