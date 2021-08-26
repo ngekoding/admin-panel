72b5233f31a1dd9ad69f6d8f392c4d9d9fe93530
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-08-26 11:55:59 +0800

    style: add global style for page example

--- src/pages/developments/icon/code-basic-usage.js
+++ src/pages/developments/icon/code-basic-usage.js
@@ -1,3 +1,6 @@
 export default 
-`<home-icon-s class="w-12 h-12 text-purple-500" />
-<home-icon-o class="w-12 h-12 text-green-500" />`
+`<div class="flex space-x-5">
+  <home-icon-o class="w-12 h-12 text-purple-500" />
+  <chart-pie-icon-s class="w-12 h-12 text-green-500" />
+  <light-bulb-icon-o class="w-12 h-12 text-yellow-500" />
+</div>`
