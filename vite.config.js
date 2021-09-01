db2efeac2ac5cdbf8416ca328e42a16981e82256
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-09-01 12:03:24 +0800

    perf: optimize tailwind for production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents from 'vite-plugin-components'
+import { visualizer } from 'rollup-plugin-visualizer'
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -40,6 +41,11 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 3000
+    },
+    build: {
+      rollupOptions: {
+        plugins: [visualizer()]
+      }
     }
   }
 })
